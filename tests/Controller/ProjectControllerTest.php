49369e4e329785034d46303579c82e5c2bcf5d91
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2022-10-22 18:34:39 +0300

    project controller

---
+++ tests/Controller/ProjectControllerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProjectControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private ProjectRepository $repository;
+    private string $path = '/project/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient([], [
+            'HTTP_HOST'       => 'localhost:8000'
+        ]);
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Project::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Project index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'project[name]' => 'Testing',
+            'project[max_students_per_group]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/project/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Project();
+        $fixture->setName('My Title');
+        $fixture->setMax_students_per_group('My Title');
+
+        $this->repository->add($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Project');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Project();
+        $fixture->setName('My Title');
+        $fixture->setMax_students_per_group('My Title');
+
+        $this->repository->add($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'project[name]' => 'Something New',
+            'project[max_students_per_group]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/project/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getName());
+        self::assertSame('Something New', $fixture[0]->getMax_students_per_group());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Project();
+        $fixture->setName('My Title');
+        $fixture->setMax_students_per_group('My Title');
+
+        $this->repository->add($fixture, true);
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/project/');
+    }
+}
